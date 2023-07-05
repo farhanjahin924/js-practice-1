6af8fc3cfe1ea7cb6ed7cfe5dabc5ec80a297a77
Farhan Jahin <farhan15-13948@diu.edu.bd>
2023-07-06 02:13:51 +0600

    resolved problems

--- hello World !/script.js
+++ hello World !/script.js
@@ -1,13 +1,13 @@
-//  let hwb = document.querySelector("button");
-// hwb.addEventListener('click',showmsg);
+ let hwb = document.querySelector(".b1");
+hwb.addEventListener('click',showmsg);
 
-// function showmsg()
-// {
-// alert("You did it Farhan! welcome from Js🙌 ")
+function showmsg()
+{
+alert("You did it Farhan! welcome from Js🙌 ")
 
-// }
+}
 
-let change = document.querySelector("button");
+let change = document.querySelector(".b2");
 change.addEventListener('click', change_msg)
 function change_msg()
 {
